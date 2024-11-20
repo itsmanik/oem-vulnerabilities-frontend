e0cbbd94f803296ed093fff2512af88ec138eadd
itsmanik <manik.hilalpure@gmail.com>
2024-11-20 14:29:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TopNav from "./components/Nav/TopNav/TopNav";
+
+const App = () => {
+  return (
+    <div>
+      <TopNav />
+      <h1 className="text-3xl font-bold underline">...</h1>
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Nav/TopNav/TopNav.jsx
@@ -0,0 +1,10 @@
+import classes from "./TopNav.module.css";
+
+const TopNav = () => {
+  return (
+    <>
+      <h1 className={classes.topNav}>mean ass ananya</h1>
+    </>
+  );
+};
+export default TopNav;
